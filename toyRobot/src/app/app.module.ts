a460e6b589a6ecb6190b1ee0e05a4e7adf2b25d6
biswalsiddhartha <siddharthab1995@gmail.com>
2020-05-24 03:11:29 +0530

    commit 5

--- toyRobot/src/app/app.module.ts
+++ toyRobot/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ToyMovementComponent } from './toy-movement/toy-movement.component';
@@ -13,7 +13,7 @@ import { ToyMovementComponent } from './toy-movement/toy-movement.component';
 @NgModule({
   declarations: [AppComponent,ToyMovementComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
--- toyRobot/src/app/toy-movement/toy-movement.component.ts
+++ toyRobot/src/app/toy-movement/toy-movement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-toy-movement',
@@ -7,8 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToyMovementComponent implements OnInit {
 
-  constructor() { }
+  ToyDetails:FormGroup
+  xCoordinate:number
+  yCoordinate:number
+  direction:string
+  constructor(private FBObject:FormBuilder) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.ToyDetails=this.FBObject.group({
+      xCoordinate:[null,[Validators.required]],
+      yCoordinate:[null,[Validators.required]],
+      direction:[null,[Validators.required]],
+   
+    })  
+    
+  }
+
+  leftMove(){
+    alert("left is clicked")
+  }
+  rightMove(){
+    
+  }
+  submit(){
+    alert("submit is clicked");
+    this.xCoordinate=this.ToyDetails.controls.xCoordinate.value;
+    this.yCoordinate=this.ToyDetails.controls.yCoordinate.value;
+    this.direction=this.ToyDetails.controls.direction.value;
+  this.result()
+  }
+
+  result(){
+    console.log("x"+this.xCoordinate)
+    console.log("y"+this.yCoordinate)
+    console.log("direction"+this.direction)
+  }
 
 }
